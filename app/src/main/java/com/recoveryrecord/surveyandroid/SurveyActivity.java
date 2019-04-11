232ac7607be6077b0df2103af276ad5cc3512886
nmullaney <nora.mullaney@gmail.com>
2019-04-11 10:42:26 -0700

    Refactor Activities; add Example Questions

---
+++ app/src/main/java/com/recoveryrecord/surveyandroid/SurveyActivity.java
@@ -0,0 +1,7 @@
+package com.recoveryrecord.surveyandroid;
+
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SurveyActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/recoveryrecord/surveyandroid/example/ExampleSurveyActivity.java
@@ -0,0 +1,15 @@
+package com.recoveryrecord.surveyandroid.example;
+
+import android.os.Bundle;
+
+import com.recoveryrecord.surveyandroid.R;
+import com.recoveryrecord.surveyandroid.SurveyActivity;
+
+public class ExampleSurveyActivity extends SurveyActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example_survey);
+    }
+}
