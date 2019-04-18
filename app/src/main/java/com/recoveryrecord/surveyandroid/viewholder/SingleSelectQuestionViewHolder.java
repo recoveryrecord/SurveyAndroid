06c9e43fd2ab910a4947f46c6ac2b3b7667970c4
nmullaney <nora.mullaney@gmail.com>
2019-04-18 14:42:24 -0700

    Allow multiple other fields in SingleSelectQuestion

--- app/src/main/java/com/recoveryrecord/surveyandroid/viewholder/SingleSelectQuestionViewHolder.java
+++ app/src/main/java/com/recoveryrecord/surveyandroid/viewholder/SingleSelectQuestionViewHolder.java
@@ -2,13 +2,13 @@ package com.recoveryrecord.surveyandroid.viewholder;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 
 import com.recoveryrecord.surveyandroid.R;
 import com.recoveryrecord.surveyandroid.question.Option;
@@ -16,7 +16,6 @@ import com.recoveryrecord.surveyandroid.question.OtherOption;
 import com.recoveryrecord.surveyandroid.question.SingleSelectQuestion;
 
 public class SingleSelectQuestionViewHolder extends QuestionViewHolder<SingleSelectQuestion> {
-
 
     private RadioGroup answerSelector;
     private ViewGroup otherSection;
@@ -39,14 +38,26 @@ public class SingleSelectQuestionViewHolder extends QuestionViewHolder<SingleSel
             final RadioButton radioButton = new RadioButton(getContext());
             radioButton.setText(option.title);
             if (option instanceof OtherOption) {
-                radioButton.setId(R.id.other_button);
+                radioButton.setTag(R.id.is_other, Boolean.TRUE);
             }
             answerSelector.addView(radioButton);
         }
         answerSelector.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                otherSection.setVisibility(checkedId == R.id.other_button ? View.VISIBLE : View.GONE);
+                RadioButton selectedButton = answerSelector.findViewById(checkedId);
+                if (selectedButton.getTag(R.id.is_other) != null && selectedButton.getTag(R.id.is_other) == Boolean.TRUE) {
+                    otherSection.setVisibility(View.VISIBLE);
+                } else {
+                    otherSection.setVisibility(View.GONE);
+                }
+
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNext();
             }
         });
 
@@ -57,5 +68,10 @@ public class SingleSelectQuestionViewHolder extends QuestionViewHolder<SingleSel
         super.resetState();
         answerSelector.removeAllViews();
         editOther.setText(null);
+    }
+
+    private void onNext() {
+        // TODO
+        Log.d("SingleSelectQuestion", "Clicked onNext");
     }
 }
