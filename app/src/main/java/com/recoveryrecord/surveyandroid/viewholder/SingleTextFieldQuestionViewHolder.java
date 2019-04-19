f3d5be3d0aed3e23d7aaf23ad2a260c8df660ecf
nmullaney <nora.mullaney@gmail.com>
2019-04-19 13:12:54 -0700

    Add QuestionState to SingleTextField

--- app/src/main/java/com/recoveryrecord/surveyandroid/viewholder/SingleTextFieldQuestionViewHolder.java
+++ app/src/main/java/com/recoveryrecord/surveyandroid/viewholder/SingleTextFieldQuestionViewHolder.java
@@ -4,8 +4,9 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
 import android.text.InputType;
-import android.util.Log;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -15,11 +16,15 @@ import android.widget.TextView;
 import com.recoveryrecord.surveyandroid.QuestionState;
 import com.recoveryrecord.surveyandroid.R;
 import com.recoveryrecord.surveyandroid.question.SingleTextFieldQuestion;
+import com.recoveryrecord.surveyandroid.util.SimpleTextWatcher;
 
 public class SingleTextFieldQuestionViewHolder extends QuestionViewHolder<SingleTextFieldQuestion> {
+    private static final String EDIT_TEXT_KEY = "edit_text";
+    private static final String ANSWER_ON_EDIT_UPDATE_KEY = "answer_on_edit_update_key";
 
     private TextInputLayout answerInputLayout;
     private TextInputEditText answerEdit;
+    private TextWatcher editTextWatcher;
     private Button nextButton;
 
     public SingleTextFieldQuestionViewHolder(Context context, @NonNull View itemView) {
@@ -27,27 +32,10 @@ public class SingleTextFieldQuestionViewHolder extends QuestionViewHolder<Single
 
         answerInputLayout = itemView.findViewById(R.id.answer_input_layout);
         answerEdit = itemView.findViewById(R.id.answer_edit);
-        answerEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_NEXT) {
-                    onNext();
-                    return true;
-                }
-                return false;
-            }
-        });
-
         nextButton = itemView.findViewById(R.id.next_button);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onNext();
-            }
-        });
     }
 
-    public void bind(SingleTextFieldQuestion question, QuestionState questionState) {
+    public void bind(final SingleTextFieldQuestion question, final QuestionState questionState) {
         super.bind(question);
         answerInputLayout.setHint(question.label);
         if (question.maxChars != null) {
@@ -57,20 +45,52 @@ public class SingleTextFieldQuestionViewHolder extends QuestionViewHolder<Single
         if (question.input_type != null) {
             answerEdit.setInputType(question.input_type.equals("number") ? InputType.TYPE_CLASS_NUMBER : InputType.TYPE_CLASS_TEXT);
         }
+        answerEdit.setText(questionState.get(EDIT_TEXT_KEY));
+        editTextWatcher = new SimpleTextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                questionState.put(EDIT_TEXT_KEY, s.toString());
+                if (questionState.get(ANSWER_ON_EDIT_UPDATE_KEY, false)) {
+                    questionState.setAnswer(s.toString());
+                }
+            }
+        };
+        answerEdit.addTextChangedListener(editTextWatcher);
+        answerEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                    onNext(questionState);
+                    return true;
+                }
+                return false;
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNext(questionState);
+            }
+        });
     }
 
     @Override
     protected void resetState() {
         super.resetState();
+        if (editTextWatcher != null) {
+            answerEdit.removeTextChangedListener(editTextWatcher);
+        }
+        answerEdit.setOnEditorActionListener(null);
         answerEdit.setText(null);
         answerEdit.setInputType(InputType.TYPE_CLASS_TEXT);
         answerInputLayout.setHint(null);
         answerInputLayout.setCounterEnabled(false);
-
+        nextButton.setOnClickListener(null);
     }
 
-    private void onNext() {
-        // TODO
-        Log.d("SingleTextField", "Clicked onNext");
+    private void onNext(QuestionState questionState) {
+        // TODO validations
+        questionState.setAnswer(answerEdit.getText().toString());
+        questionState.put(ANSWER_ON_EDIT_UPDATE_KEY, true);
     }
 }
